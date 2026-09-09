6e446e45a2ccb523b73541826f8020eff8a0b80d
Ravikiran Chavan <varsha.khese@hindindustec.com>
2026-09-10 01:19:51 +0530

    resolve error

--- dentalcareUI/src/app/shared/services/websiteDetails.service.ts
+++ dentalcareUI/src/app/shared/services/websiteDetails.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { WebsiteDetails } from '@shared/models/websiteDetails.model';
 import { DoctorSlot } from 'app/core/services/rdv-utils.service';
+import { environment } from 'environments/environment';
 import { Observable } from 'rxjs';
 
 @Injectable({
